ef593e45d1e1b33cd89286845cd1550224ea13ae
sakuarmoika <sai.kumarkan27@gmail.com>
2024-06-11 12:09:35 +0530

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,7 @@
+const containerEl = document.querySelector(".container");
+const careers = ["YouTube", "Web Developer", "Frelancer"];
+let careerIndex = 0;
+
+function updateText() {
+  containerEl.innerHTML = <h1>I Am Sai Kumar $(careera[3])</h1>;
+}
